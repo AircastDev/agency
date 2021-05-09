46d095ffa8e57a9feacdd7f2903b8c0fab2a1969
James Birtles <jamesb@airca.st>
2021-05-09 13:59:57 +0200

    add ability to create actor with context + add priority message queue for self sent messages

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,38 @@
-//
-// - Each actor has its own 'thread'
-// - Actor has a run method that is called in a loop
-// - Actor can select over incoming messages and any other async stream-like thing
-
 use async_executors::{TokioTp, TokioTpBuilder};
-use async_nursery::{NurseExt, Nursery, NurseryStream};
+use async_nursery::{NurseExt, Nursery};
 pub use async_trait::async_trait;
+use std::{collections::VecDeque, future::Future};
 use tokio::sync::mpsc;
 
 #[async_trait]
 pub trait Actor: Send + Sync + Sized {
     type Msg: Send + Sync;
 
-    async fn run(&mut self, mailbox: &mut Context<Self>);
+    async fn run(&mut self, ctx: &mut Context<Self>);
+
+    async fn stopped(&mut self, _ctx: &mut Context<Self>) {}
 }
 
+#[async_trait]
+pub trait Setup: Actor {
+    type Args: Send + Sync;
+
+    async fn setup(ctx: &mut Context<Self>, args: Self::Args) -> Option<Self>;
+}
+
+#[derive(Clone)]
 pub struct Agency {
     nursery: Nursery<TokioTp, ()>,
-    nursery_stream: NurseryStream<()>,
 }
 
 impl Agency {
-    pub fn new() -> Self {
+    pub fn new() -> (Self, impl Future<Output = ()>) {
         let mut exec_builder = TokioTpBuilder::new();
         exec_builder.tokio_builder().enable_all();
         let exec = exec_builder.build().expect("create tokio threadpool");
 
         let (nursery, nursery_stream) = Nursery::new(exec);
-        Agency {
-            nursery,
-            nursery_stream,
-        }
+        (Agency { nursery }, nursery_stream)
     }
 
     pub fn hire<A>(&self, mut actor: A) -> Addr<A>
@@ -41,27 +43,48 @@ impl Agency {
         let addr = Addr::new(sender);
         let mut ctx = Context {
             mailbox: receiver,
+            priority: VecDeque::new(),
             stopped: false,
             addr: addr.clone(),
+            agency: self.clone(),
         };
         self.nursery
             .nurse(async move {
                 while !ctx.stopped {
                     actor.run(&mut ctx).await;
                 }
+
+                actor.stopped(&mut ctx).await;
             })
             .unwrap();
         addr
     }
 
-    pub async fn wait(self) {
-        self.nursery_stream.await;
-    }
-}
+    pub fn hire_with<A>(&self, args: A::Args) -> Addr<A>
+    where
+        A: 'static + Setup,
+    {
+        let (sender, receiver) = mpsc::channel(16);
+        let addr = Addr::new(sender);
+        let mut ctx = Context {
+            mailbox: receiver,
+            priority: VecDeque::new(),
+            stopped: false,
+            addr: addr.clone(),
+            agency: self.clone(),
+        };
+        self.nursery
+            .nurse(async move {
+                if let Some(mut actor) = A::setup(&mut ctx, args).await {
+                    while !ctx.stopped {
+                        actor.run(&mut ctx).await;
+                    }
 
-impl Default for Agency {
-    fn default() -> Self {
-        Self::new()
+                    actor.stopped(&mut ctx).await;
+                }
+            })
+            .unwrap();
+        addr
     }
 }
 
@@ -131,8 +154,10 @@ where
     A: Actor,
 {
     mailbox: mpsc::Receiver<A::Msg>,
+    priority: VecDeque<A::Msg>,
     stopped: bool,
     addr: Addr<A>,
+    pub agency: Agency,
 }
 
 impl<A> Context<A>
@@ -141,6 +166,10 @@ where
 {
     /// Pull the next message off the stack, waiting if there are none
     pub async fn message(&mut self) -> Option<A::Msg> {
+        if let Some(msg) = self.priority.pop_back() {
+            return Some(msg);
+        }
+
         self.mailbox.recv().await
     }
 
@@ -150,5 +179,12 @@ where
 
     pub fn address(&self) -> Addr<A> {
         self.addr.clone()
+    }
+
+    /// Send a message back to this actor.
+    ///
+    /// Messages sent this way take priority over regular messages.
+    pub fn notify(&mut self, msg: A::Msg) {
+        self.priority.push_front(msg);
     }
 }
